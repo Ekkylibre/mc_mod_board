12cabf0cb6572e3099d9bcee34df0e6048485949
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-09-22 15:38:09 +0200

    🍱 Theme > Colors & Border > Define color palette and border styles

---
+++ src/app/theme/index.tsx
@@ -0,0 +1,16 @@
+export const colors = {
+    background: "#16181c",
+    "background raised": "#26292f",
+    "lighter background raised": "#33363d",
+    "button border": "#c1bed1",
+    text: "#b0bac5",
+    "darker text": "#7e858e",
+    "brand color": "#d9a61a",
+    "lighter brand color": "#dcae2f",
+};
+
+export const border = {
+    sizeIcons: "6rem",
+    borderRadiusIcons: "calc(var(--size) * 0.16667)",
+    sizeRoundedCard: "1rem",
+};
