70a9181f02df8aa8f1de248b7bed98755aff7fad
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-09-22 17:52:42 +0200

    ✨ Components > AddServerButton > Create component

---
+++ src/app/components/AddServerButton.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { colors } from '../theme';
+
+const StyledButton = styled.button`
+  background-color: ${colors['lighter brand color']};
+  color:  ${colors.background};
+  border: none;
+  border-radius: 10px;
+  width: 100%;
+  height: 60px;
+  font-size: 16px;
+  cursor: pointer;
+  padding: 10px;
+`;
+
+export default function AddServerButton() {
+  return (
+    <StyledButton>+</StyledButton>
+  )
+}
