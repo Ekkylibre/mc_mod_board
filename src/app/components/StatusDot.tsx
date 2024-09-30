252be80f408fcf77d5b2a2b6293b363ab8bb7db2
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-09-30 19:44:19 +0200

    ✨ Components > StatusDot > Create component

---
+++ src/app/components/StatusDot.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Dot = styled.div<{ $isActive: boolean }>`
+  width: 12px; /* ajustez la taille si nécessaire */
+  height: 12px;
+  border-radius: 50%;
+  background-color: ${(props) => (props.$isActive ? 'green' : 'red')};
+  display: inline-block;
+`;
+
+type DotProps = {
+    isActive: boolean;
+}
+
+const StatusDot: React.FC<DotProps> = ({ isActive }) => {
+    return <Dot $isActive={isActive} />;
+};
+
+export default StatusDot;
